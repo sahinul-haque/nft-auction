1fa3fa5a57a263a3e82a4512b81f3cd6cef711a4
Sahinul Haque <sahinul.haque@itpeoplecorp.com>
2022-03-30 17:09:22 +0530

    display 15 blocks in screen
    Signed-off-by: Sahinul Haque <sahinul.haque@itpeoplecorp.com>

--- auction-ui/src/components/BlockchainExplorer.js
+++ auction-ui/src/components/BlockchainExplorer.js
@@ -56,13 +56,12 @@ class BlockchainExplorer extends Component {
       creatorId: parsedBlock.txs[0].creator_msp_id,
     };
     let blocks = this.state.blocks;
-    
+
     //display 15 blocks
     if(blocks.length==15) {
-      blocks.splice(0,1, newBlock);
-    } else {
-      blocks.push(newBlock);
-    }
+      blocks.splice(0,1);
+    } 
+    blocks.push(newBlock);
     
     this.setState({ blocks });
     $(function () {
